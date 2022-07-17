4185c0ad062e5716f505399e622587a47de20b75
Malzagic <przemek2142@gmail.com>
2022-07-18 00:35:47 +0200

    changing js function and new varables little of changing css

--- app.js
+++ app.js
@@ -5,54 +5,59 @@ async function getData() {
   const API_URL = './data.json';
   fetch(API_URL)
     .then(response => response.json())
-    .then(data => setDate(data));
+    .then(data => setData(data));
 }
 
 
-function setDate(data) {
+function setData(data) {
 
   createElements();
 
   days.forEach((day, index) => {
+
     day.textContent = data[index].day
-  })
+
+  });
+
   bars.forEach((bar, index) => {
+
     bar.style.height = data[index].amount + '%'
 
-    bar.addEventListener('mouseover', e => {
-      const popup = document.querySelector('.popup');
-      const popupTitle = document.querySelector('.popup-title');
-
-      popup.classList.add('active')
-      popupTitle.textContent = '$' + e.target.style.height.slice(0,5);
-    });
-
-    bar.addEventListener('mouseout', (e) => {
-      const popup = document.querySelector('.popup');
-
-      if(!bar.classList.contains('today')) {
-        popup.classList.remove('active');
-      }
-    });
-  })
+  });
 
 }
 
 const createElements = () => {
+
   const div = document.createElement('div');
   const p = document.createElement('p');
   const columns = document.querySelectorAll('.column');
 
+
   columns.forEach(column => {
+
     column.addEventListener('mouseover', e => {
 
-      if(!e.target.classList.contains('column')) return;
-      e.target.appendChild(div);
-      div.classList.add('popup');
-      div.appendChild(p);
-      p.classList.add('popup-title');
+      if(e.target.classList.contains('bar')){
+        e.target.appendChild(div);
+        div.classList.add('popup', 'active');
+        div.appendChild(p);
+        p.classList.add('popup-title');
+        p.textContent = '$' + e.target.style.height.slice(0,5);
+      }
+
     });
+
+    column.addEventListener('mouseout', e => {
+
+      if(!e.target.classList.contains('bar')){
+        div.remove();
+      }
+
+    });
+
   });
+
 }
 
 window.onload = function todayDay() {
@@ -60,6 +65,24 @@ window.onload = function todayDay() {
   const today = date.getDay();
 
   bars[today].classList.add('today');
+
+  const spans = document.querySelectorAll('.bar');
+  const div = document.createElement('div');
+  const p = document.createElement('p');
+
+  spans.forEach(span => {
+
+      if(span.classList.contains('today')) {
+
+      span.appendChild(div);
+      div.classList.add('popup', 'active');
+      div.appendChild(p);
+      p.classList.add('popup-title');
+      p.textContent = '$' + span.style.height.slice(0,5);
+
+    }
+
+  });
 }
 
 getData();
