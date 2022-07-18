4c2802a35301730e9e2cb9cb7708282e9654b01f
Malzagic <przemek2142@gmail.com>
2022-07-18 21:14:35 +0200

    repair reload data for todayDay

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ window.onload = function todayDay() {
       div.appendChild(p);
       p.classList.add('popup-title');
       p.textContent = '$' + span.style.height.slice(0,5);
-
+      if(p.textContent === '$') todayDay();
     }
 
   });
